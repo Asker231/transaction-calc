1a054cd9760bad9ec27d6986fb0ca04663f50479
Asker <kotsevasker0@gmail.com>
2024-08-26 16:53:33 +0300

    create func ScanTransaction

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
+import "fmt"
+
 
 	func main() {
-		
+		var transactions []float64 = []float64{}
+		for{
+			fmt.Print("Your transaction:")
+			transaction := ScanTransaction()
+			transactions = append(transactions, transaction)
+			if transaction == 0{
+				fmt.Println(transactions)
+				break
+			}
+		}
+	}
+
+	func ScanTransaction()float64{
+		var transaction int
+		fmt.Scan(&transaction)
+		return float64(transaction)
 	}
